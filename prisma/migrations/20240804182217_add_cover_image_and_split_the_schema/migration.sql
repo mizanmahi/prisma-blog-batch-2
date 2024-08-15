3d70e818bb35858b59303cd71bccdfe795c49459
mizanmahi <mizanmahi24@gmail.com>
2024-08-15 14:47:42 +0600

    mergin to main with my desktop

--- prisma/migrations/20240804182217_add_cover_image_and_split_the_schema/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "blogs" ADD COLUMN     "coverImage" TEXT;
--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-import { Prisma, PrismaClient } from '@prisma/client';
-import bcrypt from 'bcrypt';
-
-const prisma = new PrismaClient();
-
-const createUser = async (data: any) => {
-   const hashedPassword = await bcrypt.hash(data.password, 10);
-
-   const createdUserData = await prisma.user.create({
-      data: { ...data.user, password: hashedPassword },
-   });
-   return createdUserData;
-};
-
-const getUsersFromDB = async (queryParams: any) => {
-   /* 
-   //!using full text search, won't work for multiple field as need raw sql for that (unsupported database features)
-   return await prisma.user.findMany({
-      where: {
-         username: {
-            search: queryParams.q,
-         },
-      },
-      select: { id: true, username: true, email: true, isActive: true },
-   }); 
-   */
-
-   const { q, ...otherQueryParams } = queryParams;
-
-   const conditions: Prisma.UserWhereInput[] = [];
-
-   const searchableFields = ['username', 'email'];
-
-   //@ for searching on multiple fields
-   // if (queryParams.q) {
-   //    conditions.push({
-   //       OR: searchableFields.map((field) => ({
-   //          [field]: {
-   //             contains: queryParams.q,
-   //          },
-   //       })),
-   //    });
-   // }
-
-   // ! refactored
-   if (q) {
-      const searchConditions = searchableFields.map((field) => ({
-         [field]: { contains: q },
-      }));
-      conditions.push({ OR: searchConditions });
-=======
 import { Admin, Author, UserRole } from '@prisma/client';
 import bcrypt from 'bcrypt';
 import prisma from '../../../shared/prismaClient';
@@ -59,21 +7,10 @@ const createAdmin = async (req: any): Promise<Admin> => {
    if (req.file) {
       const uploadedFile = await fileUploader.saveToCloudinary(req.file);
       req.body.admin.profilePhoto = uploadedFile?.secure_url;
->>>>>>> 55c7b29d44141deb21190edd8c78333fcdfedac0
    }
 
    const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-<<<<<<< HEAD
-   return await prisma.user.findMany({
-      where: { AND: conditions },
-   });
-};
-
-export const userService = {
-   createUser,
-   getUsersFromDB,
-=======
    const userData = {
       email: req.body.admin.email,
       password: hashedPassword,
@@ -102,7 +39,7 @@ const createAuthor = async (req: any): Promise<Author> => {
    const userData = {
       email: req.body.author.email,
       password: hashedPassword,
-      role: UserRole.AUTHOR,
+      role: UserRole.BLOGGER,
    };
 
    const result = await prisma.$transaction(async (txClient) => {
@@ -120,5 +57,4 @@ const createAuthor = async (req: any): Promise<Author> => {
 export const userService = {
    createAdmin,
    createAuthor,
->>>>>>> 55c7b29d44141deb21190edd8c78333fcdfedac0
 };
