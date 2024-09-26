36f7bd36b8a86f0e5bf627daa34c374fb2eb1200
mizanmahi <mizanmahi24@gmail.com>
2024-09-27 00:27:01 +0600

    remove: dist folder

--- dist/app/modules/admin/admin.constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validParams = exports.searchableFields = void 0;
-exports.searchableFields = ['name', 'email'];
-exports.validParams = ['q', 'name', 'email'];
